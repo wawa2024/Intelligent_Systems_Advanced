2f28ba72819ac55eac895ccec2b641ece8a4ee6c
wawa2024 <156529019+wawa2024@users.noreply.github.com>
2024-02-08 17:00:22 +0200

    src/main/: 	- ArduinoCode 		Renamed ArduinoCode to 'src' convention 	+ src/main.ino 		Added template code and macro header 	+ src/pin.h 		Created a macro for arduino NANO pins

---
+++ src/main/main.ino
@@ -0,0 +1,19 @@
+/* 
+ * Display 16x4 Library: 	liquid-crystal	
+ * 
+ *	
+ *	
+ */
+
+#include "pins.h"
+
+void setup() {
+//	pinMode(PIN(X),OUTPUT);
+	
+}
+
+void loop() {
+
+	#define DELAY
+//	delay(DELAY);
+}
