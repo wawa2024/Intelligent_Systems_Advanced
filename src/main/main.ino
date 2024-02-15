74613a710c58c2d3a5449c8c3d754b8dd80844f7
Juho Rantsi <juho.rantsi@tuni.fi>
2024-02-15 15:05:55 +0200

    analog sensor code added

--- src/main/main.ino
+++ src/main/main.ino
@@ -5,15 +5,29 @@
  *	
  */
 
-#include "pins.h"
+//#include "pins.h"
+
+#define DELAY 100
+#define MAX_VOLTAGE 5
+
+int WindDirection(int pin, int reference=1023){
+
+    int var = analogRead(pin);
+
+    int voltage = var * ( MAX_VOLTAGE / reference ) ;
+
+    int wind_direction = map(voltage, 0 , 3.8 , 0 , 359 );
+
+    return wind_direction ;
+}
 
 void setup() {
 //	pinMode(PIN(X),OUTPUT);
-	
+
+	  pinMode(A3,INPUT);
 }
 
 void loop() {
-
-	#define DELAY
-//	delay(DELAY);
+	Serial.println(WindDirection(A3));
+	delay(DELAY);
 }
