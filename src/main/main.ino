fe3641f71735c8ea47d70470dc4030bbae89b8b9
wawa2024 <156529019+wawa2024@users.noreply.github.com>
2024-02-17 17:54:31 +0200

    src/main/main.ino:     + Added WindSpeed direction method     + Created global namespaces for 'var' and 'pin'     + Created generic abstraction functions for printing and       initialization

--- src/main/main.ino
+++ src/main/main.ino
@@ -5,35 +5,105 @@
  *	
  */
 
-//#include "pins.h"
+    //////////////////////
+    // Global Variables //
+    //////////////////////
 
-#define DELAY 100
-#define VOLTAGE_REF 5
+namespace var
+{
+    int refresh_rate    = 100,
+        baud_rate       = 9600,
+        ref_voltage     = 5,
+        wind_speed      = 0
+        ;
+}
 
+namespace pin
+{
+    int wind_direction = A4,
+        wind_speed     = 2
+        ;
+}
 
-double mapd(double x, double in_min, double in_max, double out_min, double out_max){     
+    ////////////////////
+    // Wind Direction //
+    ////////////////////
+
+double mapd(double x, double in_min, double in_max, double out_min, double out_max)
+{     
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; 
 }
 
+int WindDirection(int pin)
+{
+    static const int Max_ADC_Ret = 1023;
 
-int WindDirection(int pin, int Max_ADC_Ret = 1023){
-
-    int var = analogRead(pin);
-
-    double voltage = (double)var * ( (double)VOLTAGE_REF / (double)Max_ADC_Ret ) ;
+    double voltage = (double)analogRead(pin) * 
+                     ( 
+                        (double)var::ref_voltage / 
+                        (double)Max_ADC_Ret 
+                     );
 
     int wind_direction = mapd(voltage, 0 , 3.8 , 0 , 359 );
 
     return wind_direction ;
 }
 
-void setup() {
-//	pinMode(PIN(X),OUTPUT);
-    Serial.begin(9600);
-	pinMode(A4,INPUT);
+void PrintWindDirection(void)
+{
+    Serial.print("Wind Direction: ");
+    Serial.println(WindDirection(pin::wind_direction));
 }
 
-void loop() {
-	Serial.println(WindDirection(A4));
-	delay(DELAY);
+    ////////////////
+    // Wind Speed //
+    ////////////////
+
+void WindSpeed(void)
+{
+    var::wind_speed += digitalRead(pin::wind_speed);
+}
+
+void PrintWindSpeed(void)
+{
+    Serial.print("Wind Speed: ");
+    Serial.println(var::wind_speed);
+    var::wind_speed = 0 ;
+}
+
+    //////////
+    // Main //
+    //////////
+
+void PrintStats(void)
+{
+    PrintWindDirection();
+    PrintWindSpeed();
+}
+
+void InitPins(int* a, int size, int mode)
+{
+    for(int i=0 ; i < size ; i++)
+        pinMode(a[i],mode);
+}
+
+void setup()
+{
+    int a[] = { pin::wind_direction , pin::wind_speed };
+
+    InitPins(a,sizeof(a)/sizeof(int),INPUT);
+
+    Serial.begin(var::baud_rate);
+
+    attachInterrupt(
+        digitalPinToInterrupt(pin::wind_speed),
+        WindSpeed,
+        RISING
+    );
+}
+
+void loop()
+{
+    PrintStats();
+	delayMicroseconds(var::refresh_rate);
 }
