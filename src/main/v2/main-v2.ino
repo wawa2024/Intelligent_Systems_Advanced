abcb004d847bc03dc832f454035f46603e5e65e8
wawa2024 <156529019+wawa2024@users.noreply.github.com>
2024-03-07 01:49:37 +0200

    src/main/v2: Added a rudementary Info object

--- src/main/v2/main-v2.ino
+++ src/main/v2/main-v2.ino
@@ -17,9 +17,26 @@ void PrintStats(void)
     WindSpeed::Print();
 }
 
+struct Info
+{
+    int GetRam(void)
+    {
+        extern int __heap_start, *__brkval;
+        int v;
+        return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
+    }
+    void PrintRam(void)
+    {
+        Serial.print("Arduino::RAM: ");
+        Serial.println(GetRam());
+    }
+    Info(){ Serial.begin(baud_rate); PrintRam(); }
+    ~Info(){ PrintRam(); }
+};
+
 void setup()
 {
-    Serial.begin(baud_rate);
+    Info observer; 
     WindSpeed::Init();
     WindDirection::Init();
 }
