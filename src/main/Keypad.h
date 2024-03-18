bed5d3c89f7ad0ecca751f76bc5c0ed55e65927a
wawa2024 <156529019+wawa2024@users.noreply.github.com>
2024-03-19 00:37:04 +0200

    src/main: Keypad.h     + Added a timed Interrupt through TimerInterrupt.h library.     + Minor logical additions.

--- src/main/Keypad.h
+++ src/main/Keypad.h
@@ -1,38 +1,37 @@
 /******************************************************************************
  * File: ./Keypad.h
- * Dependencies: avr/interrupt.h
+ * Dependencies: <TimerInterrupt.h>
+ * Upstream: https://github.com/khoih-prog/TimerInterrupt
+ * Alternative: <avr/interrupt.h>
  ******************************************************************************/
-//#include <avr/interrupt.h> //pending
 
 namespace Keypad
 {
     constexpr float V(float x){ return ( ( 5.0 / 16.0 ) * x ); }
-    struct {
-        int 
+    const struct {
+        constexpr static int 
             input[1]  = { A6 },
             output[0] = {}
             ;
     } pin ;
 
-    struct {
-        const int 
+    const struct {
+        constexpr static int 
             keycodes = 4 * 4,
             bus_size = 1,
             ref_voltage = 5
             ;
         const float 
-            field[16] = {
+            field[keycodes] = {
                       V(1)  ,   V(2)    ,   V(3)    ,   V(4),
                       V(5)  ,   V(6)    ,   V(7)    ,   V(8),
                       V(9)  ,   V(10)   ,   V(11)   ,   V(12),
                       V(13) ,   V(14)   ,   V(15)   ,   V(16)
             };
-        const float
-            offset = V(1) / 2
-            ;
+        const float offset = V(1) / 2;
     } env ;
 
-    int keycode = 0;
+    volatile int keycode = 0;
     struct Key { 
         const char* name; 
         void (*handler)(void); 
@@ -56,10 +55,10 @@ namespace Keypad
         { "A" , NULL }
     };
 
-    inline float Voltage()
+    volatile float Voltage()
     {
         float input = analogRead(pin.input[0]);
-        float voltage = input * ( (float)env.ref_voltage / (float)1023 );
+        volatile float voltage = input * ( (float)env.ref_voltage / (float)1023 );
 
         #define DEBUG
         #ifdef DEBUG
@@ -71,29 +70,24 @@ namespace Keypad
         return voltage;
     }
 
-    void Scan(void)
+
+    #include <setjmp.h>
+    jmp_buf stack;
+    int state;
+
+    void ScanKeys(void)
     {
-        float voltage = Voltage();
-        static bool state = true;
-       
-        // Maybe add a delayed repeat rate functionality
-        // static unsigned long t_press = 0;
-        // unsigned long t_time = millis();
-        //
-        // A timed Interrupt handler could be used to
-        // scan for keypress changes, which when read
-        // trigger a flag which is used within all LCD
-        // program bodies to determine if they should halt their
-        // execution. The interrupt handler would only change
-        // 2 values, keycode (through Scan()) and a flag value
-
-        for(int i=0 ; i < env.keycodes ; i++)
+        volatile float voltage = Voltage();
+        if( env.offset < voltage )
         {
-            float f = env.field[i];
-
-            float low = f - env.offset, high = f + env.offset;
-            if( ( low < voltage ) && ( high > voltage ) )
-                keycode = i + 1; 
+            for(int i=0 ; i < env.keycodes ; i++)
+            {
+                const float& f = env.field[i];
+                volatile float low = f - env.offset, high = f + env.offset;
+                if( ( low < voltage ) && ( voltage < high ) )
+                    keycode = i + 1; 
+            }
+            longjmp(stack,state);
         }
     }
 
@@ -117,10 +111,34 @@ namespace Keypad
 
     void Exec(void)
     {
-        Scan(); Task();
+        state=setjmp(stack); interrupts() ; Task();
     }
 
     void AttatchHandler(int i,void (*p)(void)){key[i-1].handler=p;}
+    void AttatchKeys(void)
+    {
+        AttatchHandler(1,LCD::Draw::Stats);
+        AttatchHandler(2,LCD::Draw::Alphabet);
+    }
+
+
+    #define USE_TIMER_1 true
+    #include <TimerInterrupt.h>
+
+    void AttatchTimer(void)
+    {
+        const unsigned long refresh_rate_milliseconds = 200;
+        ITimer1.init();
+        if(ITimer1.attachInterruptInterval(refresh_rate_milliseconds,ScanKeys))
+        {
+            Serial.print("Starting  ITimer1 OK, millis() = "); 
+            Serial.println(millis());
+        }
+        else
+            Serial.println(F("Can't set ITimer1. Select another freq. or timer"));
+
+    }
+
     void Init(void)
     {
         for(int i=0; i < env.bus_size ; i++)
@@ -128,11 +146,13 @@ namespace Keypad
             pinMode(pin.input[i] , INPUT);
             pinMode(pin.output[i] , OUTPUT);
         }
+
         #ifdef DEBUG
         Debug::dump_var(env.offset,"Offset");
         Debug::dump_array(env.field,16,"Field");
         #endif
-        AttatchHandler(1,LCD::Draw::Stats);
-        AttatchHandler(2,LCD::Draw::Alphabet);
+        
+        AttatchKeys();
+        AttatchTimer();
     }
 }
