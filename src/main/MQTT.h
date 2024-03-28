b320343b947a60d3d2bdc87f12cfbb86b8d26756
wawa2024 <156529019+wawa2024@users.noreply.github.com>
2024-03-28 13:45:38 +0200

    ...

--- src/main/MQTT.h
+++ src/main/MQTT.h
@@ -11,44 +11,37 @@ namespace MQTT
 
     uint16_t port = 1883;
     uint8_t ip [4] = { 10,6,0,21 };
-
-    char* clientId = "a731fsd4";
-
     struct {
         char* in = "ICT4_in_2020";
         char* out = "ICT4_out_2020";
     } topic ;
 
+    char* clientId = "a731fsd4";
+    PubSubClient client( ip, port, NET::interface );
+
     void Callback(char*,uint8_t*,uint8_t);
 
-    PubSubClient* client = 00;
-
-    bool Error(void)
+    void Error(void)
     {
         Serial.println("MQTT failed to connect");
         return false;
     }
 
-    void Connect(void)
-    {
-        Serial.println("MQTT connecting...");
-        if( client->connect( clientId ) )
-        {
-            Serial.println("MQTT connection established");
-        } else {
-            Error();
-        }
-    }
-
     void Send(void)
     {
-        if( not client -> connected() )
+        if( not ( client.connected() ) )
         {
-            Connect();
+            Serial.println("MQTT connecting...");
+            if(  client.connect( clientId ) )
+            {
+                Serial.println("MQTT connection established");
+            } else {
+                Error();
+            }
         }
-        if( client -> connected() ) {
+        if( client.connected() ) {
             Serial.println("Sending MQTT package");
-            client -> publish( topic.out , buf );
+            client.publish( topic.out , buf );
         } else {
             Error();
         }
@@ -75,12 +68,6 @@ namespace MQTT
 
     void Init(void)
     {
-        Serial.println("MQTT initialization");
-        if(not client)
-        {
-            static PubSubClient host( ip, port, NET::interface );
-            client = &host;
-            Serial.println("MQTT initialized");
-        }
+        Serial.println("MQTT initialized");
     }
 }
