63efb37cba67d2c5e44c61bfd68e51e93de34df6
lamiika <47070356+lamiika@users.noreply.github.com>
2024-04-09 15:07:26 +0300

    Changed note division into hardcoded note duration for memory savings

--- src/main/include/MusicPlayer.h
+++ src/main/include/MusicPlayer.h
@@ -4,16 +4,9 @@ namespace MusicPlayer
   #include "Pitches.h"
 
   uint8_t pin = 6;
-  uint32_t whole_note = 4*60/100*1000;
   uint32_t previousMillis = 0;
 
-  void setTempo(uint32_t value) {
-    whole_note = 4*60/value*1000;
-  }
-
-  void playNote(uint8_t pin, uint16_t frequency, float division) {
-    uint32_t duration = whole_note / division;
-
+  void playNote(uint8_t pin, uint16_t frequency, uint16_t duration) {
     uint32_t actualDuration = duration * 0.9;
     
     if (frequency > 0) {
@@ -26,7 +19,6 @@ namespace MusicPlayer
   }
 
   void playSong() {
-    setTempo(grace[0].frequency);
     uint16_t repetitions = grace[0].repetitions;
     size_t song_length = sizeof(grace) / sizeof(Note);
 
@@ -34,7 +26,7 @@ namespace MusicPlayer
       for (int note_index = 1; note_index < song_length; note_index++) {
         // 0 index used for metadata ^
         for (int repeat_note = 0; repeat_note < grace[note_index].repetitions; repeat_note++) {
-          playNote(pin, grace[note_index].frequency, grace[note_index].division);
+          playNote(pin, grace[note_index].frequency, grace[note_index].duration);
         }
       }
     }
