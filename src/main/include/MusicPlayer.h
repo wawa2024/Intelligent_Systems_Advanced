a8d097d4848ddc0852f6e9dcf2ef4d75bc634ecf
lamiika <47070356+lamiika@users.noreply.github.com>
2024-04-10 15:45:31 +0300

    Included main branch music player optimizations into the experimental branch

--- src/main/include/MusicPlayer.h
+++ src/main/include/MusicPlayer.h
@@ -3,16 +3,9 @@ namespace MusicPlayer
   #include "Song.h"
 
   uint8_t pin = 6;
-  uint32_t whole_note = 4*60/100*1000;
   uint32_t previousMillis = 0;
 
-  void setTempo(uint32_t value) {
-    whole_note = 4*60/value*1000;
-  }
-
-  void playNote(uint8_t pin, uint16_t frequency, float division) {
-    uint32_t duration = whole_note / division;
-
+  void playNote(uint8_t pin, uint16_t frequency, uint16_t duration) {
     uint32_t actualDuration = duration * 0.9;
     
     if (frequency > 0) {
@@ -25,16 +18,13 @@ namespace MusicPlayer
   }
 
   void playSong() {
-    setTempo(grace[0].frequency);
-    uint8_t repetitions = grace[0].repetitions;
+    uint16_t repetitions = grace[0].duration;
     size_t song_length = sizeof(grace) / sizeof(Note);
 
-    for (uint8_t times_played = 0; times_played < repetitions; times_played++) {
-      for (size_t note_index = 1; note_index < song_length; note_index++) {
+    for (int times_played = 0; times_played < repetitions; times_played++) {
+      for (int note_index = 1; note_index < song_length; note_index++) {
         // 0 index used for metadata ^
-        for (uint8_t repeat_note = 0; repeat_note < grace[note_index].repetitions; repeat_note++) {
-          playNote(pin, grace[note_index].frequency, grace[note_index].division);
-        }
+        playNote(pin, grace[note_index].frequency, grace[note_index].duration);
       }
     }
   }
